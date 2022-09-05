6d5b70255fc749b6a3ee590324aadd88d87a5655
Sergei Efimov <efimovsergeiv@gmail.com>
2022-09-05 17:14:05 +0300

    add: carousel

--- components/sections/Main.vue
+++ components/sections/Main.vue
@@ -1,31 +1,52 @@
 <template>
-  <section id="" class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+  <div>
+    <div class="mb-10">
+      <h1>Hallo welt</h1>
+    </div>
+      <div class="carousel-wrapper">
 
-    <h1>Main section</h1>
+        <carousel v-bind="options">
+          <slide v-for="i in 10" :key="i" class="img-wrapper">
+            <img :src="`https://picsum.photos/800/400/?image=4${i}`" />
+          </slide>
 
-  </section>
+          <numbered-pagination slot="pagination" />
+          <stylish-navigation slot="navigation" />
+        </carousel>
+
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'MainSection',
-  components: {
-    // Hooper,
-  },
-  props: {
-    cts: {
-      type: Array,
-      default: Array,
-    },
-    banners: {
-      type: Array,
-      default: Array,
-    },
-  },
   data() {
     return {
-      dataset: true,
+      options: {
+        loop: true,
+        perPage: 6,
+        paginationEnabled: true,
+        paginationColor: "#ffffff",
+        paginationEnabled: true
+      }
     }
-  },
+  }
 }
 </script>
+
+<style>
+.carousel-wrapper {
+  padding: 20px;
+  height: 400px;
+
+}
+.VueCarousel-slide {
+  text-align: center;
+}
+.img-wrapper img {
+  margin: auto;
+  width: 300px;
+  height: 400px;
+  background-image: linear-gradient(gray 100%, transparent 0);
+}
+</style>
