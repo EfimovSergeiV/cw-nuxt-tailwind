f98254d1b9b0cb72eba8c3d3ff125cd85f5dafeb
Sergei Efimov <efimovsergeiv@gmail.com>
2022-09-07 16:55:40 +0300

    dev: cts

--- pages/cts.vue
+++ pages/cts.vue
@@ -1,23 +1,70 @@
 <template>
-    <div>
-      <h1>Категории</h1>
+    <div class="mx-auto py-2 px-4 lg:max-w-7xl lg:px-8">
 
-      <nuxt-link to="products/page" class="bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium">LIST RPODUCTS</nuxt-link>
+      <!-- <nuxt-link to="products/page" class="bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium">LIST RPODUCTS</nuxt-link> -->
 
-      <small>{{ cts }}</small>
-  
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+
+        <div v-for="ct in cts" :key="ct.id" class="flex items-stretch bg-white rounded-sm border dark:border-gray-700 dark:bg-gray-800 shadow-md p-2">
+
+            
+
+            <div class="flex flex-col">
+              <div>
+                <p>{{ ct.name }}</p>
+              </div>
+             
+
+              <div class="flex flex-col">
+                <div v-for="sct in ct.inserted" :key="sct.id" >
+                  <p>{{ sct.name }}</p>
+                </div>
+              </div>
+
+            </div>
+          
+          <!-- <div class="">
+            <p v-for="sct in ct.inserted" :key="sct.id">{{ sct.name }}</p>
+          </div> -->
+          
+        </div>
+
+
+
+          <!-- <div class="">
+            <div class="bg-white rounded-sm border dark:border-gray-700 dark:bg-gray-800 shadow-md">
+              <div class="">
+              <div class="p-2">
+                <p class="p-1">{{ ct.name }}</p>
+
+                <div class="" v-for="sct in ct.inserted" :key="sct.id">
+                  <p class="p-2">{{ sct.name }}</p>
+                </div>
+
+              </div>
+            </div>
+            
+            </div>
+          </div> -->
+
+
+      </div>
+
+
+
+
     </div>
   </template>
   
-  <script>
-import Products from '~/components/sections/Products.vue'
+<script>
+  import Products from '~/components/sections/Products.vue'
 //   import Main from '~/components/sections/Main.vue'
 
   export default {
     name: 'CtsPage',
     components: {
-    Products
-},
+      Products
+    },
     async asyncData({ $axios }) {
       const cts = await $axios.$get(`c/ct/`)
       return { cts }
@@ -28,4 +75,4 @@ import Products from '~/components/sections/Products.vue'
       }
     },
   }
-  </script>
+</script>
