d49bea70397cb06c8bf90d121ce4d9c30e5806e0
Sergei Efimov <efimovsergeiv@gmail.com>
2022-09-08 12:32:02 +0300

    dev: cts

--- pages/cts.vue
+++ pages/cts.vue
@@ -1,51 +1,36 @@
 <template>
     <div class="mx-auto py-2 px-4 lg:max-w-7xl lg:px-8">
 
-      <!-- <nuxt-link class="bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium">LIST RPODUCTS</nuxt-link> -->
-
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+      <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
 
         <div v-for="ct in cts" :key="ct.id" class="flex items-stretch bg-white rounded-sm border dark:border-gray-700 dark:bg-gray-800 shadow-md p-2">
 
             
 
-            <div class="flex flex-col">
-              <div>
-                <nuxt-link to="products/page" class="text-gray-400 hover:text-white px-3 py-2 rounded-md text-sm font-medium">{{ ct.name }}</nuxt-link>
+            <div class="w-full">
+              <div class="">
+                <nuxt-link to="products/page" class="text-gray-400 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  <h2>{{ ct.name }}</h2>
+                </nuxt-link>
               </div>
              
 
               <div class="flex flex-col">
                 <div v-for="sct in ct.inserted" :key="sct.id" >
-                  <p>{{ sct.name }}</p>
+                  <div>
+                    <nuxt-link to="#" class="text-gray-400 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                      {{ sct.name }}
+                    </nuxt-link>
+                  </div>
+                  
                 </div>
               </div>
 
             </div>
-          
-          <!-- <div class="">
-            <p v-for="sct in ct.inserted" :key="sct.id">{{ sct.name }}</p>
-          </div> -->
+
+            
           
         </div>
-
-
-
-          <!-- <div class="">
-            <div class="bg-white rounded-sm border dark:border-gray-700 dark:bg-gray-800 shadow-md">
-              <div class="">
-              <div class="p-2">
-                <p class="p-1">{{ ct.name }}</p>
-
-                <div class="" v-for="sct in ct.inserted" :key="sct.id">
-                  <p class="p-2">{{ sct.name }}</p>
-                </div>
-
-              </div>
-            </div>
-            
-            </div>
-          </div> -->
 
 
       </div>
